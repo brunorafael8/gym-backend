4ecf7f6cf4dd95822d160cd2956d83deefff2b48
brunorafael8 <bruninho_lito@hotmail.com>
2024-02-19 21:09:52 -0300

    feat: update workout module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
+import { join } from "path";
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { WorkoutModule } from "./workout/workout.module";
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(__dirname, "./schema.gql"),
+      playground: true,
+    }),
+    WorkoutModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
